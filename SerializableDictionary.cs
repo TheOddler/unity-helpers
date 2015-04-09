eec2f8197257e69cb37d51a6088268780bf4354c
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-04-09 20:55:50 +0200

    Added a serializable dictionary.

---
+++ SerializableDictionary.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//
+// Unity doesn't know how to serialize a Dictionary
+// So this is a simple extension of a dictionary that saves as two lists.
+[System.Serializable]
+public class SerializableDictionary<TKey,TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+{
+	[SerializeField]
+	private List<TKey> _keys;
+	[SerializeField]
+	private List<TValue> _values;
+	
+	public void OnBeforeSerialize()
+	{
+		_keys = new List<TKey>(this.Count);
+		_values = new List<TValue>(this.Count);
+		foreach(var kvp in this)
+		{
+			_keys.Add(kvp.Key);
+			_values.Add(kvp.Value);
+		}
+	}
+	
+	public void OnAfterDeserialize()
+	{
+		this.Clear();
+		for (int i=0; i!= Mathf.Min(_keys.Count,_values.Count); i++) {
+			this.Add(_keys[i],_values[i]);
+		}
+	}
+}
