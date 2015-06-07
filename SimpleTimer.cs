e068b163fb5226b18f9b7c3f5df7282b32ac4a60
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-06-07 14:54:17 +0200

    Restructured files, so the editor-only helpers are in a editor folder.

---
+++ SimpleTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class SimpleTimer {
+	
+	private static System.Diagnostics.Stopwatch _stopwatch;
+	private static string _name;
+	
+	public static void Start(string name = "Timer") {
+		_name = name;
+		_stopwatch = System.Diagnostics.Stopwatch.StartNew();
+	}
+	
+	public static void Report() {
+		Debug.Log(_name + " (ms): " + _stopwatch.ElapsedMilliseconds);
+	}
+	
+	public static void Stop() {
+		_stopwatch.Stop();
+	}
+	
+}
