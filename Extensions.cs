e0a7b77f2645e15f506acc8c37ff264a56130f25
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-06-07 14:50:20 +0200

    Added some basic extensions for layermasks. Also added meta files, not sure they belong, but we'll see.

---
+++ Extensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class LayerMaskExtensions
+{
+    public static bool Contains(this LayerMask layermask, int layer)
+    {
+        return layermask == (layermask | (1 << layer));
+    }
+
+    public static bool IsEmpty(this LayerMask layermask)
+    {
+        return layermask.value == 0;
+    }
+}
