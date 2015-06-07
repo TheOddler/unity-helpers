6de9ecacd6068ac086ef2832dcf05130e522e298
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-06-07 15:17:40 +0200

    Added simple asserts.

---
+++ Assert.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+
+public static class Assert
+{
+    const string DEFAULT_MESSAGE = "Assert failed.";
+
+    public static void True(bool check, string message = DEFAULT_MESSAGE)
+    {
+        if (!check)
+        {
+            Debug.LogError(message);
+        }
+    }
+
+    public static void False(bool check, string message = DEFAULT_MESSAGE)
+    {
+        True(!check, message);
+    }
+
+    // Functions
+    public static void True(Func<bool> check, string message = DEFAULT_MESSAGE)
+    {
+        True(check(), message);
+    }
+
+    public static void False(Func<bool> check, string message = DEFAULT_MESSAGE)
+    {
+        False(check(), message);
+    }
+}
