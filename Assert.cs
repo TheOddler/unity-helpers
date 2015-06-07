687e8282f4c05b6b1882ef27a01f7719e643a5b0
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-06-07 16:41:24 +0200

    Added Equals and NotNull check to asserts.

--- Assert.cs
+++ Assert.cs
@@ -6,6 +6,7 @@ public static class Assert
 {
     const string DEFAULT_MESSAGE = "Assert failed.";
 
+    // Default
     [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
     public static void True(bool check, string message = DEFAULT_MESSAGE)
     {
@@ -21,6 +22,12 @@ public static class Assert
         True(!check, message);
     }
 
+    [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
+    public static void Equals(object obj1, object obj2, string message = DEFAULT_MESSAGE)
+    {
+        True(obj1 == obj2, message);
+    }
+
     // Functions
     [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
     public static void True(Func<bool> check, string message = DEFAULT_MESSAGE)
@@ -32,5 +39,12 @@ public static class Assert
     public static void False(Func<bool> check, string message = DEFAULT_MESSAGE)
     {
         False(check(), message);
+    }
+
+    // Objects
+    [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
+    public static void NotNull(object obj, string message = DEFAULT_MESSAGE)
+    {
+        True(obj != null, message);
     }
 }
