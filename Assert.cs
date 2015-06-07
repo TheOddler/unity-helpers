146d65e7c3a9c86127eb2edf856d747d8c0cbd43
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-06-07 15:37:01 +0200

    Added conditional attributes to the asserts, so they get omitted in release builds.

--- Assert.cs
+++ Assert.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System;
+using Conditional = System.Diagnostics.ConditionalAttribute;
 
 public static class Assert
 {
     const string DEFAULT_MESSAGE = "Assert failed.";
 
+    [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
     public static void True(bool check, string message = DEFAULT_MESSAGE)
     {
         if (!check)
@@ -13,17 +15,20 @@ public static class Assert
         }
     }
 
+    [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
     public static void False(bool check, string message = DEFAULT_MESSAGE)
     {
         True(!check, message);
     }
 
     // Functions
+    [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
     public static void True(Func<bool> check, string message = DEFAULT_MESSAGE)
     {
         True(check(), message);
     }
 
+    [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
     public static void False(Func<bool> check, string message = DEFAULT_MESSAGE)
     {
         False(check(), message);
